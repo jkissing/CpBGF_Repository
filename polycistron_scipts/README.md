dd7125e7923ec13f3657750b9e23b888c6d229de
Rui Xiao <60984362+ruicatxiao@users.noreply.github.com>
2024-12-30 11:45:51 -0500

    Delete polycistron_scipts directory

--- polycistron_scipts/README.md
+++
@@ -1,1 +0,0 @@
-polycistron_code
--- polycistron_scipts/run_sqanti3.sh
+++
@@ -1,4 +0,0 @@
-# Annotate transcript with SQANTI3
-sqanti3_qc.py <TAMA_OUTPUT.gtf> <REF_ANNOTATION.gtf> <INPUT_REF.fasta> \
--t 64 -o <OUTPUT_PREFIX> -fl <TAMA_ABUNDANCE_TABLE.txt> \
---report both --saturation
